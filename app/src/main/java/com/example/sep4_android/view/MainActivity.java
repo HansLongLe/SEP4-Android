7c9b629b8a7fb57bbe33464df9ce60acb7baac75
gof3ck69 <sorryy647@gmail.com>
2022-04-28 21:10:59 +0200

    Bottom Navigation Bar

--- app/src/main/java/com/example/sep4_android/view/MainActivity.java
+++ app/src/main/java/com/example/sep4_android/view/MainActivity.java
@@ -1,10 +1,14 @@
 package com.example.sep4_android.view;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
 
 import com.example.sep4_android.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,5 +16,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        bottomNavigation();
+    }
+
+    private void bottomNavigation() {
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+        NavController navController = Navigation.findNavController(this,  R.id.fragmentContainerViewMainActivity);
+        NavigationUI.setupWithNavController(bottomNavigationView, navController);
     }
 }
