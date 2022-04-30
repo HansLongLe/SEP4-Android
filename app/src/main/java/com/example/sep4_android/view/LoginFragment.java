f57f772eb8e312e987bbdff7204d50944739dc3b
Constantin Ginga <costelgn@gmail.com>
2022-04-30 18:35:09 +0200

    Add Create Account Fragment & Login Fragment

--- app/src/main/java/com/example/sep4_android/view/LoginFragment.java
+++ app/src/main/java/com/example/sep4_android/view/LoginFragment.java
@@ -2,6 +2,7 @@ package com.example.sep4_android.view;
 
 import androidx.lifecycle.ViewModelProvider;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -11,6 +12,8 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.sep4_android.R;
 import com.example.sep4_android.viewModel.LoginViewModel;
@@ -18,6 +21,7 @@ import com.example.sep4_android.viewModel.LoginViewModel;
 public class LoginFragment extends Fragment {
 
     private LoginViewModel mViewModel;
+    private Intent mainActivityIntent;
 
     public static LoginFragment newInstance() {
         return new LoginFragment();
@@ -26,14 +30,21 @@ public class LoginFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.login_fragment, container, false);
-    }
-
-    @Override
-    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
         mViewModel = new ViewModelProvider(this).get(LoginViewModel.class);
-        // TODO: Use the ViewModel
+        View view = inflater.inflate(R.layout.login_fragment, container, false);
+        mainActivityIntent = new Intent(getActivity(), MainActivity.class);
+        Button btn = view.findViewById(R.id.loginBtn);
+        TextView redirectLink = view.findViewById(R.id.createAccountRedirect);
+        redirectLink.setOnClickListener(v -> redirectToCreateAccount());
+        btn.setOnClickListener(v -> login());
+        return view;
     }
 
+    private void login() {
+        startActivity(mainActivityIntent);
+    }
+
+    private void redirectToCreateAccount() {
+        getParentFragmentManager().beginTransaction().replace(R.id.authFragment, new CreateAccountFragment()).commit();
+    }
 }
